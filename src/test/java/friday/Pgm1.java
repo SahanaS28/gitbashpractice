01b95ce51028aa759ecec11f84f6b577300b354d
SahanaS28 <sahanass1997@gmail.com>
2024-03-07 12:36:09 +0530

    first commit

---
+++ src/test/java/friday/Pgm1.java
@@ -0,0 +1,15 @@
+package friday;
+
+public class Pgm1
+{
+	
+  static int x=10;
+  public static void test()
+  {
+	  System.out.println(x);
+	  System.out.println(Pgm1.x); 
+	  Pgm1 p=new Pgm1();
+	  System.out.println(p.x);
+  }
+	
+}
